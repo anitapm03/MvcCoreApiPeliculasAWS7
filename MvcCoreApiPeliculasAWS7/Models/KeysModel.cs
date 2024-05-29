e3ab80ee7338423e31a1de11d56412acd70f6f0b
anitapm03 <apereiramolinero@gmail.com>
2024-05-29 10:57:46 +0200

    .

---
+++ MvcCoreApiPeliculasAWS7/Models/KeysModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MvcCoreApiPeliculasAWS7.Models
+{
+    public class KeysModel
+    {
+        public string MySql { get; set; }
+
+        public string ApiPeliculas { get; set; }
+
+        public string BucketName { get; set; }
+
+        public string BucketUrl { get; set; }
+
+    }
+}
